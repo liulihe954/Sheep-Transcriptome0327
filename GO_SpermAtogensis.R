33ba5eb9c6b868b79e4eaeecfdf7621f73f91e2b
Lihe Liu <lihe.liu@ufl.edu>
2020-10-09 02:15:24 -0500

    up

--- GO_SpermAtogensis.R
+++ GO_SpermAtogensis.R
@@ -94,68 +94,68 @@ get_NC_cor = function(datExpr1,datExpr2,r_thres,p_thres){
   Results = list(NC1 = NC1, NC2 = NC2, cormatr_ref = cormatr1, cormatr_test = cormatr2, pvalue1 = pvalmatr1,pvalue2 = pvalmatr2 ,adjmatr_ref = adjmatr1, adjmatr_test = adjmatr2, basic = basic_results,change = change_results)
   return(Results)
 }
-# get_NC_pcor = function(datExpr1,datExpr2,r_thres, p_thres){
-  cormatr1 <- cor(datExpr1)
-  adjmatr1 = matrix(1,ncol(datExpr1),ncol(datExpr1))
-  
-  adjmatr1[abs(cormatr1) < r_thres] = 0
-  #calcu of matrx2
-  cormatr2 <- cor(datExpr2)
-  adjmatr2 = matrix(1,ncol(datExpr2),ncol(datExpr2))
-  
-  adjmatr2[abs(cormatr2) < r_thres] = 0
-  #use threshold
-  for(i in 2:ncol(datExpr1)){
-    r1 = cor.test(datExpr1[,i-1],datExpr1[,i])
-    r2 = cor.test(datExpr2[,i-1],datExpr2[,i])
-    if(r1$p.value >= p_thres){adjmatr1[i-1,i] = adjmatr1[i,i-1] = 0}
-    if(r2$p.value >= p_thres){adjmatr2[i-1,i] = adjmatr2[i,i-1] = 0}
-  }
-  
-  #calcu of matrx1
-  cormatr1 <- pcor(datExpr1)$estimate
-  adjmatr1 = matrix(1,ncol(datExpr1),ncol(datExpr1))
-  colnames(adjmatr1) = colnames(datExpr1)
-  rownames(adjmatr1) = colnames(datExpr1)
-  adjmatr1[abs(cormatr1) < r_thres] = 0
-  # calcu of matrx2
-  cormatr2 <- pcor(datExpr2)$estimate
-  adjmatr2 = matrix(1,ncol(datExpr2),ncol(datExpr2))
-  colnames(adjmatr2) = colnames(datExpr2)
-  rownames(adjmatr2) = colnames(datExpr2)
-  adjmatr2[abs(cormatr2) < r_thres] = 0
-  # use threshold
-  for(i in 2:ncol(datExpr1)){
-    #  r1 = pcor(datExpr1[,i-1],datExpr1[,i])$p.value
-    #  r2 = pcor(datExpr2[,i-1],datExpr2[,i])$p.value
-    if(cormatr1[i-1,i] >= p_thres){adjmatr1[i-1,i] = adjmatr1[i,i-1] = 0}
-    if(adjmatr2[i-1,i] >= p_thres){adjmatr2[i-1,i] = adjmatr2[i,i-1] = 0}
-  }
-  #get all the basic NC
-  NC1 = conformityBasedNetworkConcepts(adjmatr1)
-  NC2 = conformityBasedNetworkConcepts(adjmatr2)
-  #combine, rank and show
-  basic_results = data.frame(density1 = NC1$fundamentalNCs$Density,
-                             density2 = NC2$fundamentalNCs$Density,
-                             centralization1 = NC1$fundamentalNCs$Centralization,
-                             centralization2 = NC2$fundamentalNCs$Centralization,
-                             heterogeneity1 = NC1$fundamentalNCs$Heterogeneity,
-                             heterogeneity2 = NC2$fundamentalNCs$Heterogeneity)
-  change_results = data.frame(#gene = colnames(datExpr1),
-                              con_1 = NC1$fundamentalNCs$Connectivity,
-                              scl_con_1 = NC1$fundamentalNCs$ScaledConnectivity,
-                              con_2 = NC2$fundamentalNCs$Connectivity,
-                              scl_con_2 = NC2$fundamentalNCs$ScaledConnectivity,
-                              con_change = -(NC1$fundamentalNCs$Connectivity - NC2$fundamentalNCs$Connectivity),
-                              scl_con_change = -(NC1$fundamentalNCs$ScaledConnectivity - NC2$fundamentalNCs$ScaledConnectivity),
-                              rank_scl_con = rank(-abs(NC1$fundamentalNCs$ScaledConnectivity-NC2$fundamentalNCs$ScaledConnectivity)),
-                              cls_coef_1 = NC1$fundamentalNCs$ClusterCoef,
-                              cls_coef_2 = NC2$fundamentalNCs$ClusterCoef,
-                              clst_coef_change = c(NC1$fundamentalNCs$ClusterCoef - NC2$fundamentalNCs$ClusterCoef),
-                              rank_clstcoef = rank(-abs(NC1$fundamentalNCs$ClusterCoef-NC2$fundamentalNCs$ClusterCoef)))
-  Results = list(NC1 = NC1, NC2 = NC2, cormatr_ref = cormatr1, cormatr_test = cormatr2, adjmatr_ref = adjmatr1, adjmatr_test = adjmatr2, basic = basic_results,change = change_results)
-  return(Results)
-}
+#  get_NC_pcor = function(datExpr1,datExpr2,r_thres, p_thres){
+#   cormatr1 <- cor(datExpr1)
+#   adjmatr1 = matrix(1,ncol(datExpr1),ncol(datExpr1))
+#   
+#   adjmatr1[abs(cormatr1) < r_thres] = 0
+#   #calcu of matrx2
+#   cormatr2 <- cor(datExpr2)
+#   adjmatr2 = matrix(1,ncol(datExpr2),ncol(datExpr2))
+#   
+#   adjmatr2[abs(cormatr2) < r_thres] = 0
+#   #use threshold
+#   for(i in 2:ncol(datExpr1)){
+#     r1 = cor.test(datExpr1[,i-1],datExpr1[,i])
+#     r2 = cor.test(datExpr2[,i-1],datExpr2[,i])
+#     if(r1$p.value >= p_thres){adjmatr1[i-1,i] = adjmatr1[i,i-1] = 0}
+#     if(r2$p.value >= p_thres){adjmatr2[i-1,i] = adjmatr2[i,i-1] = 0}
+#   }
+#   
+#   #calcu of matrx1
+#   cormatr1 <- pcor(datExpr1)$estimate
+#   adjmatr1 = matrix(1,ncol(datExpr1),ncol(datExpr1))
+#   colnames(adjmatr1) = colnames(datExpr1)
+#   rownames(adjmatr1) = colnames(datExpr1)
+#   adjmatr1[abs(cormatr1) < r_thres] = 0
+#   # calcu of matrx2
+#   cormatr2 <- pcor(datExpr2)$estimate
+#   adjmatr2 = matrix(1,ncol(datExpr2),ncol(datExpr2))
+#   colnames(adjmatr2) = colnames(datExpr2)
+#   rownames(adjmatr2) = colnames(datExpr2)
+#   adjmatr2[abs(cormatr2) < r_thres] = 0
+#   # use threshold
+#   for(i in 2:ncol(datExpr1)){
+#     #  r1 = pcor(datExpr1[,i-1],datExpr1[,i])$p.value
+#     #  r2 = pcor(datExpr2[,i-1],datExpr2[,i])$p.value
+#     if(cormatr1[i-1,i] >= p_thres){adjmatr1[i-1,i] = adjmatr1[i,i-1] = 0}
+#     if(adjmatr2[i-1,i] >= p_thres){adjmatr2[i-1,i] = adjmatr2[i,i-1] = 0}
+#   }
+#   #get all the basic NC
+#   NC1 = conformityBasedNetworkConcepts(adjmatr1)
+#   NC2 = conformityBasedNetworkConcepts(adjmatr2)
+#   #combine, rank and show
+#   basic_results = data.frame(density1 = NC1$fundamentalNCs$Density,
+#                              density2 = NC2$fundamentalNCs$Density,
+#                              centralization1 = NC1$fundamentalNCs$Centralization,
+#                              centralization2 = NC2$fundamentalNCs$Centralization,
+#                              heterogeneity1 = NC1$fundamentalNCs$Heterogeneity,
+#                              heterogeneity2 = NC2$fundamentalNCs$Heterogeneity)
+#   change_results = data.frame(#gene = colnames(datExpr1),
+#                               con_1 = NC1$fundamentalNCs$Connectivity,
+#                               scl_con_1 = NC1$fundamentalNCs$ScaledConnectivity,
+#                               con_2 = NC2$fundamentalNCs$Connectivity,
+#                               scl_con_2 = NC2$fundamentalNCs$ScaledConnectivity,
+#                               con_change = -(NC1$fundamentalNCs$Connectivity - NC2$fundamentalNCs$Connectivity),
+#                               scl_con_change = -(NC1$fundamentalNCs$ScaledConnectivity - NC2$fundamentalNCs$ScaledConnectivity),
+#                               rank_scl_con = rank(-abs(NC1$fundamentalNCs$ScaledConnectivity-NC2$fundamentalNCs$ScaledConnectivity)),
+#                               cls_coef_1 = NC1$fundamentalNCs$ClusterCoef,
+#                               cls_coef_2 = NC2$fundamentalNCs$ClusterCoef,
+#                               clst_coef_change = c(NC1$fundamentalNCs$ClusterCoef - NC2$fundamentalNCs$ClusterCoef),
+#                               rank_clstcoef = rank(-abs(NC1$fundamentalNCs$ClusterCoef-NC2$fundamentalNCs$ClusterCoef)))
+#   Results = list(NC1 = NC1, NC2 = NC2, cormatr_ref = cormatr1, cormatr_test = cormatr2, adjmatr_ref = adjmatr1, adjmatr_test = adjmatr2, basic = basic_results,change = change_results)
+#   return(Results)
+# }
 
 ######   apply function to the dataset datExprGT is reference and datExprGO is the test testset  #####
 ######   For the results we have 
